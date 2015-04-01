1e438dc640383f5686f6c6186a4d7a8ae50a6d30
George Stocker <george@jewliebots.com>
2015-04-01 14:46:41 -0400

    more dev work on shared build script.

--- build.sh
+++ build.sh
@@ -19,8 +19,18 @@ esac
 shift
 done
 
+DEPLOY_DIRECTORY="/var/src/www"
+
 if ${ENVIRONMENT = "dev"} 
+	if [-d "$DEPLOY_DIRECTORY"]; then
+	    cp ./www $DEPLOY_DIRECTORY
+	    else
+	    mkdir /var/src/www
+            cp ./www $DEPLOY_DIRECTORY
+	fi
+	cd $DEPLOY_DIRECTORY
 	find . -type f -exec sed -i '' 's/\{{MIN}}//g {} +
+	
 fi
 if ${ENVIRONMENT = "prod"}
 	find . -type f -name '*.css' -printf '%h\n' | sort | uniq | while read file
@@ -35,12 +45,25 @@ if ${ENVIRONMENT = "prod"}
 		yuicompressor -o '.js$:-min.js' *.js
 		cd -
 	done
-	find . -type f -name '*.html' -exec sed -i '' 's/\{{MIN}}/-min/g {} +
+	
 	current_build =$(date +%s)
-	find . -type f -name '*.html' -exec sed -i '' 's/\{{VERSION}}/current_build/g {} +
+	current_tag = `date +%Y.%m.%d.%H%M`
+	find . -type f -name '*.html' -exec sed -i '' 's/\{{VERSION}}/$current_build/g {} +
 	git add .
+        git tag -a $ $current_tag -m 'Production deployment build $current_tag'
 	git commit -m "generating build for version $current_build"
-        git push	
+	git push origin $current_tag
+
+
+      if [-d "$DEPLOY_DIRECTORY"]; then
+      	cp ./www $DEPLOY_DIRECTORY
+      else
+        mkdir /var/src/www
+        cp ./www $DEPLOY_DIRECTORY
+      fi
+      cd $DEPLOY_DIRECTORY
+      find . -type f -name '*.html' -exec sed -i '' 's/\{{MIN}}/-min/g {} +
+      sudo service nginx restart
 fi
 
 
