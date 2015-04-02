95d12171023fd463261111c4a49d7693cb644008
George Stocker <george@jewliebots.com>
2015-04-02 09:54:36 -0400

    whew, script 'works'.

--- build.sh
+++ build.sh
@@ -22,38 +22,35 @@ done
 DEPLOY_DIRECTORY="/var/src/www"
 
 if [ $ENVIRONMENT = "dev" ]; then 
-      	mkdir -p $DEPLOY_DIRECTORY && cp ./www -R $DEPLOY_DIRECTORY
+      	mkdir -p $DEPLOY_DIRECTORY && cp -R ./www $DEPLOY_DIRECTORY
 	cd $DEPLOY_DIRECTORY
-	find . -type f -exec sed -i.bak '' 's/\{{MIN}}//g' {} +
+	find . -type f -name '*.html' -exec sed -i .bak 's/{{MIN}}//g' {} +
 fi
 if [ $ENVIRONMENT == "prod" ]; then
-	find . -type f -name '*.css' -printf '%h\n' | sort | uniq | while read file
-	do
-		cd $file
-		yuicompressor -o '.css$:-min.css' *.css
-		cd -
-	done
-	find . -type f -name '*.js' -printf '%h\n' | sort | uniq | while read file
-	do
-		cd $file
-		yuicompressor -o '.js$:-min.js' *.js
-		cd -
-	done
-	
-	current_build =$(date +%s)
-	current_tag = `date +%Y.%m.%d.%H%M`
-	find . -type f -name '*.html' -exec sed -i '' 's/\{{VERSION}}/$current_build/g {} +
-	git add .
-        git tag -a $ $current_tag -m 'Production deployment build $current_tag'
-	git commit -m "generating build for version $current_build"
-	git push origin $current_tag
+    find . -type f -name '*.css' -printf '%h\n' | sort | uniq | while read file
+    do
+    cd $file
+    yuicompressor -o '.css$:-min.css' *.css
+    cd -
+    done
+    find . -type f -name '*.js' -printf '%h\n' | sort | uniq | while read file
+    do
+    cd $file
+    yuicompressor -o '.js$:-min.js' *.js
+    cd -
+    done
 
-
-      	mkdir -p $DEPLOY_DIRECTORY && cp ./www -R $DEPLOY_DIRECTORY
-      fi
-      cd $DEPLOY_DIRECTORY
-      find . -type f -name '*.html' -exec sed -i.bak '' 's/\{{MIN}}/-min/g' {} +
-      sudo service nginx restart
+    current_build =$(date +%s)
+    current_tag = `date +%Y.%m.%d.%H%M`
+    find . -type f -name '*.html' -exec sed -i .bak 's/{{VERSION}}/$current_build/g' {} +
+    git add .
+    git tag -a $ $current_tag -m 'Production deployment build $current_tag'
+    git commit -m "generating build for version $current_build"
+    git push origin $current_tag
+   
+    mkdir -p $DEPLOY_DIRECTORY && cp -R ./www/ $DEPLOY_DIRECTORY
+    cd $DEPLOY_DIRECTORY
+    find . -type f -name '*.html' -exec sed -i .bak 's/{{MIN}}/\-min/g' {} +
 fi
 
 
