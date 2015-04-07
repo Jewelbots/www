15a5a6df2428bdebfd6fb5c55f1798bfc113d8a3
George Stocker <george@jewliebots.com>
2015-04-07 17:16:15 -0400

    updating deploy script

--- build.sh
+++ build.sh
@@ -28,10 +28,6 @@ if [ $ENVIRONMENT = "dev" ]; then
         find . -type f -name '*.html' -exec sed -i .bak 's/{{VERSION}}/1/g' {} +
 fi
 if [ $ENVIRONMENT == "prod" ]; then
-    source_dir="$PWD"
-    mkdir -p $DEPLOY_DIRECTORY && cp -R ./www $DEPLOY_DIRECTORY
-    cd $DEPLOY_DIRECTORY
-    cd ./www
     gfind . -type f -name '*.css' -printf '%h\n' | sort | uniq | while read file
     do
     cd $file
@@ -47,16 +43,13 @@ if [ $ENVIRONMENT == "prod" ]; then
 
     current_build=$(date +%s)
     current_tag=`date +%Y.%m.%d.%H%M`
-    cd "$source_dir"
     find . -type f -name '*.html' -exec sed -i .bak 's/{{MIN}}/\-min/g' {} +
     find . -type f -name '*.html' -exec sed -i .bak "s/{{VERSION}}/$current_build/g" {} +
     
-
-    git tag -a $current_tag -m "Production deployment build $current_tag file version: $current_build"
+    git checkout -b $current_build
     git add .
-    git commit -m "generating build for version $current_build"
-    git push origin tag $current_tag
-    git checkout $current_tag
+    git commit -m "generating build for version $current_tag and branch: $current_build"
+    git push origin $current_build
     eb deploy jewliebots-dev
 fi
 git checkout master
