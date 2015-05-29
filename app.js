08ecddf1a5082b2d2cd76b9f1610b6dd04ff4a36
George Stocker <george@jewelbots.com>
2015-05-29 16:38:01 -0400

    more moves because. Also fixed Mailchimp issues.

---
+++ app.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var flash = require('connect-flash');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
+var mcapi = require('./node_modules/mailchimp-api/mailchimp');
+
+var app = express();
+
+mc = new mcapi.Mailchimp('fea3465fb94788aaa891cf145f90c432-us3');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(__dirname + '/dist/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(flash());
+
+app.use('/', routes);
+app.post('/subscribe', function (req, res) {
+  mc.lists.subscribe(
+      {
+        id: '419ec4b9b5',
+        email: {email: req.body.EMAIL}
+      } ,
+      function(data) {
+        res.redirect('/newsletter_thankyou.html');
+      },
+      function(error) {
+        if (error.error) {
+          req.flash = error.code + ": " + error.error;
+          console.log(error);
+        } else {
+          console.log(error);
+          req.flash = "There was an error subscribing the user";
+        }
+        res.redirect('/privacy.html');
+      }
+  );
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
