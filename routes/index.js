f65735595ab5acb22573d5711f9aba9f4ad07c1f
George Stocker <gjstocker@gmail.com>
2015-12-17 19:02:06 -0500

    adding intro to wearables kit redirect

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/kickstarter', function(req,res) {
-  res.redirect('https://www.kickstarter.com/projects/1345510482/jewelbots-friendship-bracelets-that-teach-girls-to')
+  res.redirect('https://www.kickstarter.com/projects/1345510482/jewelbots-friendship-bracelets-that-teach-girls-to');
+});
+router.get('/intro-to-wearables-kit', function(req, res) {
+  res.redirect('/intro-to-wearables-kit.html');
 });
 module.exports = router;
